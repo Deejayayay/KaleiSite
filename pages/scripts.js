858e54d6f8c2dc74017004c95532a72510982a5a
Deejayayay <olad@spu.edu>
2023-11-04 02:27:54 -0700

    added fiter
    added filter and item search to shop

--- pages/scripts.js
+++ pages/scripts.js
@@ -2,3 +2,41 @@
 
 
 //for sidebar
+
+
+//for filters
+let products = {
+    data: [
+        {
+            productName: "Pearl Earings",
+            category: "Earings",
+            price: "$XX",
+            image: "pear-ear.jpeg"
+        },
+        {
+            productName: "Ring",
+            category: "Rings",
+            price: "$XX",
+            image: "pearl.jpeg"
+            },
+    ],
+};
+
+for(let i of products.data) {
+    //create card
+    let card = document.createElement("div");
+    //card category
+    card.classList.add("card", "i.category");
+
+    //img div
+    let imgContainer = document.createElement("div");
+    imgContainer.classList.add("image-container");
+
+    //img tag
+    let image = document.createElement("img");
+    img.setAttribute("src", i.image);
+    imgContainer.appendChild(image);
+    card.appendChild(imgContainer);
+
+    document.getElementById("products").appendChild(card)
+} 
