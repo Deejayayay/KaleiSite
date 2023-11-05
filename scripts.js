e47a376402df9955db3c02f1d901abd5ce6f1834
Deejayayay <olad@spu.edu>
2023-11-05 03:08:03 -0800

    Completed basic code to shop page
    Added filters, search and dynamically adding the products
    
    TODO: create database for client's products and comments

--- scripts.js
+++ scripts.js
@@ -5,41 +5,89 @@
 
 
 //for filters
+function displayProducts(){
 
-let products = {
-    data: [
+    let productData = [
         {
             productName: "Pearl Earings",
             category: "Earings",
             price: "$XX",
             image: "/img/redhoodie.jpg"
-        },
-        {
-            productName: "Ring",
-            category: "Rings",
-            price: "$XX",
-            image: "/img/redhoodie.jpg"
-            },
-    ],
-};
-
-for(let i of products.data) {
-    let card = document.createElement("div");
-
-    card.classList.add("card", "i.category");
-
-    let imgContainer = document.createElement("div");
-    imgContainer.classList.add("image-container");
-
-    let image = document.createElement("img");
-    image.setAttribute("src", i.image);
-    imgContainer.appendChild(image);
-    card.appendChild(imgContainer);
-
+        }
+    ];
 
     let products = document.getElementById("products");
-    products.innerHTML = card
-    document.getElementById("products").appendChild(card);
+    let html = "";
+
+    for(let i of productData){
+        html += `
+        <div class="card hide">
+            <img class="image-container" src="${i.image}" alt="${i.productName}">
+            <div class="product-info">
+                <h3 class="product-name">${i.productName}</h3>
+                <p>${i.category}</p>
+                <p>${i.price}</p>
+            </div>
+        </div>
+        `;
+    }
+    if(products) {
+        products.innerHTML = html;
+    }
+    else {
+        console.error("No products element found");
+    }
+    
 }
+
+displayProducts();
+
+function filterProduct(value) {
+    let buttons = document.querySelectorAll(".button-value");
+    buttons.forEach((button) => {
+        if(value === button.innerText){
+            button.classList.add("active");
+        } else {
+            button.classList.remove("active");
+        }
+    });
+
+    let elements = document.querySelectorAll(".card");
+
+    elements.forEach((element) => {
+        if(value === "All"){
+            element.classList.remove("hide");
+        } else if(element.querySelector("p").innerText === value){
+            element.classList.remove("hide");
+        } else {
+            element.classList.add("hide");
+        }
+    });
+}   
+
+document.getElementById("search").addEventListener
+("click", () => {
+    let searchInput = document.getElementById("search-input").value;
+    let elements = document.querySelectorAll(".product-name");
+    let cards = document.querySelectorAll(".card");
+    console.log(searchInput);
+
+    elements.forEach((element, index) => {
+        if(element.innerText.includes(searchInput)){
+            cards[index].classList.remove("hide");
+        } else {
+            cards[index].classList.add("hide");
+        }
+    });
+})
+
+//for filters
+window.onload = () => {
+    filterProduct("All");
+};
+
+
+
+    
 
 
